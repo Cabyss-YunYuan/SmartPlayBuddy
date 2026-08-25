a5d4aeaa1ee37b23310e6b347e6fea796702d653
梁紫琪 <1623895206@qq.com>
2026-08-25 16:07:01 +0800

    任务前

--- src/smartplaybuddy/config/__init__.py
+++ src/smartplaybuddy/config/__init__.py
@@ -4,8 +4,8 @@
 
 VERSION = "v0.0.1"
 
-SERVER_HOST = "http://localhost:8000"
-WS_URL = "ws://localhost:2508/ws"
+SERVER_HOST = "http://smtplay.cabyss.cn:8000"
+WS_URL = "ws://smtplay.cabyss.cn:2508/ws"
 # SERVER_HOST = "http://smtplay.cabyss.cn:8082"
 # WS_URL = "ws://smtplay.cabyss.cn:2508/server/ws"
 
